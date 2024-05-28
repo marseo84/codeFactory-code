8bd0806aeca1f9c7b546f681b7b6536d2bb40fec
marseo84 <himarseo@gmail.com>
2024-05-28 12:27:07 +0200

    catchup

---
+++ 06-JavaScript/js-01-hi-console-browseer/script.js
@@ -0,0 +1,10 @@
+let el = document.getElementById("demo");
+const age = [84, 48];
+const names = ["Juan", "Marcel"];
+
+el.innerHTML = `Hi, his name is ${names[0]}, <br>
+  he is ${age[0]} years old. <br><br>
+  My name is ${names[1]},<br>
+  I am ${age[1]} years old.  `;
+
+
---
+++ 06-JavaScript/js-02-name-age/script.js
@@ -0,0 +1,6 @@
+
+let el = document.getElementById("demo");
+
+console.log("hello from the Console.");
+el.innerHTML = "Hello from the Browser.";
+
